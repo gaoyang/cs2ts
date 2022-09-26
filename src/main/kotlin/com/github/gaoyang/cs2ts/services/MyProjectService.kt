725eb4150114a372536320a807cf8fec2b2a64d3
GitHub Action <action@github.com>
2022-09-26 14:48:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/gaoyang/cs2ts/services/MyProjectService.kt
+++ src/main/kotlin/com/github/gaoyang/cs2ts/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.gaoyang.cs2ts.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gaoyang.cs2ts.MyBundle
 
 class MyProjectService(project: Project) {
 
