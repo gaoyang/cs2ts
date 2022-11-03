fe3ae596d5a23250dcb87be18748048c3836638b
gaoyang <xz@lgysh.cn>
2022-11-03 18:08:46 +0800

    test

--- src/main/kotlin/com/github/gaoyang/cs2ts/actions/ConvertToTypescriptAction.kt
+++ src/main/kotlin/com/github/gaoyang/cs2ts/actions/ConvertToTypescriptAction.kt
@@ -1,16 +1,21 @@
 package com.github.gaoyang.cs2ts.actions
 
+import com.intellij.lang.Language
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl
 import com.intellij.openapi.project.DumbAwareAction
-import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.util.childrenOfType
+import com.intellij.testFramework.LightVirtualFile
 import com.jetbrains.rdclient.util.idea.getLanguage
 import com.jetbrains.rider.ideaInterop.fileTypes.csharp.CSharpLanguage
 import com.jetbrains.rider.ideaInterop.fileTypes.csharp.psi.impl.CSharpDummyDeclaration
 
 class ConvertToTypescriptAction : DumbAwareAction() {
+    private val myOrientation = 1
 
     override fun update(e: AnActionEvent) {
         super.update(e)
@@ -24,9 +29,14 @@ class ConvertToTypescriptAction : DumbAwareAction() {
     }
 
     override fun actionPerformed(e: AnActionEvent) {
-        val project = e.getData(PlatformDataKeys.PROJECT)
-        val editor = e.getData(PlatformDataKeys.EDITOR)
+        val project = e.getData(PlatformDataKeys.PROJECT) ?: return
         val psiFile = e.getData(CommonDataKeys.PSI_FILE) ?: return
-        var classList = psiFile.childrenOfType<CSharpDummyDeclaration>()
+        val classList = psiFile.childrenOfType<CSharpDummyDeclaration>()
+        var fileEditor = FileEditorManager.getInstance(project) as FileEditorManagerImpl
+        val language = Language.findLanguageByID("TypeScript") ?: return
+        var targetPsiFile = PsiFileFactory.getInstance(project).createFileFromText(language, "interface Dog {}")
+        var targetFile = LightVirtualFile()
+        targetFile.setContent(null, targetPsiFile.text, true)
+        fileEditor.windows.first().split(myOrientation, true, targetFile, true)
     }
 }
